90ee4f993285e02866038cf609efc6b405ee9ecd
gamgyul163 <gamgyul163@gmail.com>
2024-04-01 18:19:46 +0900

    [feat] 프로젝트 생성

---
+++ src/main/java/com/zerobase/myvod/MyvodApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.myvod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyvodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyvodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerobase/myvod/MyvodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.myvod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyvodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
